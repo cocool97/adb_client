84431f169f02307084ab7b8ec8fedb64d1ab6e64
Daerckdev <darckflix@gmail.com>
2024-06-20 13:53:45 +0200

    remove duplicate code

--- src/commands/recv.rs
+++ src/commands/recv.rs
@@ -46,28 +46,26 @@ impl AdbTcpConnection {
         // Chunk looks like 'DATA' <length> <data>
         let mut buffer = [0_u8; 64 * 1024]; // Should this be Boxed?
         let mut data_header = [0_u8; 4]; // DATA
-        let mut len_header = [0_u8; 4]; // <len>
         loop {
             self.tcp_stream.read_exact(&mut data_header)?;
-            // Check if data_header is DATA or DONE
-            if data_header.eq(b"DATA") {
-                self.tcp_stream.read_exact(&mut len_header)?;
-                let length: usize = LittleEndian::read_u32(&len_header).try_into().unwrap();
-                self.tcp_stream.read_exact(&mut buffer[..length])?;
-                output.write_all(&buffer)?;
-            } else if data_header.eq(b"DONE") {
-                // We're done here
-                break;
-            } else if data_header.eq(b"FAIL") {
-                // Handle fail
-                self.tcp_stream.read_exact(&mut len_header)?;
-                let length: usize = LittleEndian::read_u32(&len_header).try_into().unwrap();
-                self.tcp_stream.read_exact(&mut buffer[..length])?;
-                Err(RustADBError::ADBRequestFailed(String::from_utf8(
-                    buffer[..length].to_vec(),
-                )?))?;
-            } else {
-                panic!("Unknown response from device {:#?}", data_header);
+            // Check if data_header is DATA or DONE or FAIL
+            match &data_header {
+                b"DATA" => {
+                    // Handle received data
+                    let length: usize = self.get_body_length()?.try_into().unwrap();
+                    self.tcp_stream.read_exact(&mut buffer[..length])?;
+                    output.write_all(&buffer)?;
+                }
+                b"DONE" => break, // We're done here
+                b"FAIL" => {
+                    // Handle fail
+                    let length: usize = self.get_body_length()?.try_into().unwrap();
+                    self.tcp_stream.read_exact(&mut buffer[..length])?;
+                    Err(RustADBError::ADBRequestFailed(String::from_utf8(
+                        buffer[..length].to_vec(),
+                    )?))?;
+                }
+                _ => panic!("Unknown response from device {:#?}", data_header),
             }
         }
 
