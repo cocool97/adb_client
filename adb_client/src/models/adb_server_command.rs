3ff1dce4eea06629fbea688cc2685e9b70ba6937
LIAUD Corentin <corentin.liaud@orange.fr>
2024-09-13 13:48:03 +0200

    fix: minor test improvement

--- adb_client/src/models/adb_server_command.rs
+++ adb_client/src/models/adb_server_command.rs
@@ -61,8 +61,8 @@ impl Display for AdbServerCommand {
 fn test_pair_command() {
     let host = "192.168.0.197:34783";
     let code = "091102";
-    let code_u32 = code.parse::<u32>().unwrap();
-    let pair = AdbServerCommand::Pair(host.parse().unwrap(), code.into());
+    let code_u32 = code.parse::<u32>().expect("cannot parse u32");
+    let pair = AdbServerCommand::Pair(host.parse().expect("cannot parse host"), code.into());
 
     assert_eq!(pair.to_string(), format!("host:pair:{code}:{host}"));
     assert_ne!(pair.to_string(), format!("host:pair:{code_u32}:{host}"))
