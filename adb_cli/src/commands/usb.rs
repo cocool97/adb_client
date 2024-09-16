9c077dd9d4a98c13c3dd9b2a63407c8b6f57928e
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-09-16 09:56:41 +0530

    wip: temporary helper function to accept hex vids and pids

--- adb_cli/src/commands/usb.rs
+++ adb_cli/src/commands/usb.rs
@@ -1,18 +1,20 @@
 use clap::Parser;
 
+fn hex_id_to_u16(id: &str) -> Result<u16, std::num::ParseIntError> {
+    u16::from_str_radix(id, 16)
+}
+
 #[derive(Parser, Debug)]
 pub struct UsbCommand {
     /// Vendor id of this USB device
-    #[clap(short = 'v', long = "vendor-id")]
+    #[clap(short = 'v', long = "vendor-id", value_parser = hex_id_to_u16)]
     pub vendor_id: u16,
     /// Product id of this USB device
-    #[clap(short = 'p', long = "product-id")]
+    #[clap(short = 'p', long = "product-id", value_parser = hex_id_to_u16)]
     pub product_id: u16,
     // #[clap(subcommand)]
     // pub commands: UsbCommands
 }
 
 #[derive(Parser, Debug)]
-pub enum UsbCommands {
-
-}
+pub enum UsbCommands {}
