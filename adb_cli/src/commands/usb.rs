158c8ce6c34d92024034d1125576109698f955e8
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-09-18 23:53:41 +0530

    feat: parse vid and pid from hex as seen in lsusb output (#32)
    * feat: parse vid and pid from hex as seen in lsusb output

--- adb_cli/src/commands/usb.rs
+++ adb_cli/src/commands/usb.rs
@@ -1,12 +1,18 @@
+use std::num::ParseIntError;
+
 use clap::Parser;
+
+fn parse_hex_id(id: &str) -> Result<u16, ParseIntError> {
+    u16::from_str_radix(id, 16)
+}
 
 #[derive(Parser, Debug)]
 pub struct UsbCommand {
-    /// Vendor id of this USB device
-    #[clap(short = 'v', long = "vendor-id")]
+    /// Hexadecimal vendor id of this USB device
+    #[clap(short = 'v', long = "vendor-id", value_parser=parse_hex_id, value_name="VID")]
     pub vendor_id: u16,
-    /// Product id of this USB device
-    #[clap(short = 'p', long = "product-id")]
+    /// Hexadecimal product id of this USB device
+    #[clap(short = 'p', long = "product-id", value_parser=parse_hex_id, value_name="PID")]
     pub product_id: u16,
     // #[clap(subcommand)]
     // pub commands: UsbCommands
