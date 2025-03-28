7587141472dd0c159a9cda6ddb1a05ef04fe4496
Adam Gastineau <adam@agg.im>
2025-03-28 11:31:08 -0700

    Testing on macOS and Linux

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo apt-get -y install libmagic-dev
+echo "Running prebuild"
+sudo apt-get -y install libmagic-dev libudev-dev
 rm -Rf /opt/osxcross/tarballs/*
 curl -Lo /opt/osxcross/tarballs/MacOSX15.2.sdk.tar.xz "https://github.com/joseluisq/macosx-sdks/releases/download/15.2/MacOSX15.2.sdk.tar.xz"
+cd /opt/osxcross && UNATTENDED=yes OSX_VERSION_MIN=10.12 ./build.sh
+echo "Completed prebuild"
