552b3a713866116342db01e10ad1ea9c67f9b9af
Adam Gastineau <adam@agg.im>
2025-03-28 13:05:22 -0700

    More tries

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running prebuild"
-apt-get -y install libmagic-dev libudev-dev musl-tools
+apt-get -y install libmagic-dev libudev-dev musl-tools musl-dev python3
 rm -Rf /opt/osxcross/tarballs/*
 curl -Lo /opt/osxcross/tarballs/MacOSX14.0.sdk.tar.xz "https://github.com/joseluisq/macosx-sdks/releases/download/14.0/MacOSX14.0.sdk.tar.xz"
 cd /opt/osxcross && UNATTENDED=yes OSX_VERSION_MIN=10.12 ./build.sh
