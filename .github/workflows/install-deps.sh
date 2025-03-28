612e8aef75aba2d6e63ecc09561cafb86df9d8c3
Adam Gastineau <adam@agg.im>
2025-03-28 11:17:11 -0700

    Switch back to latest toolchain

--- .github/workflows/install-deps.sh
+++ .github/workflows/install-deps.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 sudo apt-get -y install libmagic-dev
+rm -Rf /opt/osxcross/tarballs/*
+curl -Lo /opt/osxcross/tarballs/MacOSX15.2.sdk.tar.xz "https://github.com/joseluisq/macosx-sdks/releases/download/15.2/MacOSX15.2.sdk.tar.xz"
