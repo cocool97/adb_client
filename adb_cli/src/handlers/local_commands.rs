f82cafb4fdf13a4f3489ec54031e587c3e87434b
LIAUD Corentin <corentinliaud26@gmail.com>
2025-04-13 17:09:22 +0200

    fix: remove duplicate list command

--- adb_cli/src/handlers/local_commands.rs
+++ adb_cli/src/handlers/local_commands.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::Write};
 
-use adb_client::{ADBListItemType, ADBServerDevice};
+use adb_client::ADBServerDevice;
 use anyhow::{anyhow, Result};
 
 use crate::models::LocalDeviceCommand;
@@ -19,25 +19,6 @@ pub fn handle_local_commands(
                 .ok_or(anyhow!("cannot list features"))?;
             log::info!("Available host features: {features}");
 
-            Ok(())
-        }
-        LocalDeviceCommand::List { path } => {
-            let dirs = device.list(path)?;
-            for dir in dirs {
-                let list_item_type = match dir.item_type {
-                    ADBListItemType::File => "File".to_string(),
-                    ADBListItemType::Directory => "Dir".to_string(),
-                    ADBListItemType::Symlink => "Symlink".to_string(),
-                };
-                log::info!(
-                    "type: {}, name: {}, time: {}, size: {}, permissions: {:#o}",
-                    list_item_type,
-                    dir.name,
-                    dir.time,
-                    dir.size,
-                    dir.permissions
-                );
-            }
             Ok(())
         }
         LocalDeviceCommand::Logcat { path } => {
