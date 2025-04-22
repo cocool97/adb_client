2dd30931f2aee2f0a0ef653cd26680fe61e413d2
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2025-04-22 02:59:23 -0400

    docs(logger): `setup_logger` safety assumptions (#107)

--- adb_cli/src/utils.rs
+++ adb_cli/src/utils.rs
@@ -1,4 +1,8 @@
-pub fn setup_logger(debug: bool) {
+/// # Safety
+///
+/// This conditionally mutates the process' environment.
+/// See [`std::env::set_var`] for more info.
+pub unsafe fn setup_logger(debug: bool) {
     // RUST_LOG variable has more priority then "--debug" flag
     if std::env::var("RUST_LOG").is_err() {
         let level = match debug {
