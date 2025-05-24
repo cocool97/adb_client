aa3943f21f781afd29d76ecee31fe6ea9a76b995
LIAUD Corentin <corentinliaud26@gmail.com>
2025-05-24 14:57:17 +0200

    wip: add wasm target support

--- adb_client/src/lib.rs
+++ adb_client/src/lib.rs
@@ -7,21 +7,37 @@
 mod adb_device_ext;
 mod constants;
 mod device;
-mod emulator_device;
 mod error;
-mod mdns;
 mod models;
-mod server;
-mod server_device;
 mod transports;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod emulator_device;
+#[cfg(not(target_arch = "wasm32"))]
+mod mdns;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod server;
+#[cfg(not(target_arch = "wasm32"))]
+mod server_device;
+#[cfg(not(target_arch = "wasm32"))]
 mod utils;
 
 pub use adb_device_ext::ADBDeviceExt;
-pub use device::{ADBTcpDevice, ADBUSBDevice};
-pub use emulator_device::ADBEmulatorDevice;
+pub use device::ADBUSBDevice;
 pub use error::{Result, RustADBError};
-pub use mdns::*;
 pub use models::{AdbStatResponse, RebootType};
+pub use transports::{ADBMessageTransport, ADBTransport};
+
+#[cfg(not(target_arch = "wasm32"))]
+pub use device::ADBTcpDevice;
+#[cfg(not(target_arch = "wasm32"))]
+pub use emulator_device::ADBEmulatorDevice;
+#[cfg(not(target_arch = "wasm32"))]
+pub use mdns::*;
+#[cfg(not(target_arch = "wasm32"))]
 pub use server::*;
+#[cfg(not(target_arch = "wasm32"))]
 pub use server_device::ADBServerDevice;
+#[cfg(not(target_arch = "wasm32"))]
 pub use transports::*;
