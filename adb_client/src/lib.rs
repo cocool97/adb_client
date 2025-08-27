8f2c3c8d8991a1b06ee6d62f0f0967a6d687a731
Corentin LIAUD <corentinliaud26@gmail.com>
2025-08-27 19:40:27 +0200

    breaking(features): add mdns feature + example

--- adb_client/src/lib.rs
+++ adb_client/src/lib.rs
@@ -3,12 +3,19 @@
 #![forbid(missing_debug_implementations)]
 #![forbid(missing_docs)]
 #![doc = include_str!("../README.md")]
+// Feature `doc_cfg` is currently only available on nightly.
+// It is activated when cfg `docsrs` is enabled.
+// Documentation can be build locally using:
+// `RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc --no-deps --all-features`
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 mod adb_device_ext;
 mod constants;
 mod device;
 mod emulator_device;
 mod error;
+#[cfg(feature = "mdns")]
+#[cfg_attr(docsrs, doc(cfg(feature = "mdns")))]
 mod mdns;
 mod models;
 mod server;
@@ -20,6 +27,8 @@ pub use adb_device_ext::ADBDeviceExt;
 pub use device::{ADBTcpDevice, ADBUSBDevice, is_adb_device, search_adb_devices};
 pub use emulator_device::ADBEmulatorDevice;
 pub use error::{Result, RustADBError};
+#[cfg(feature = "mdns")]
+#[cfg_attr(docsrs, doc(cfg(feature = "mdns")))]
 pub use mdns::*;
 pub use models::{AdbStatResponse, RebootType};
 pub use server::*;
---
+++ examples/mdns/src/main.rs
@@ -0,0 +1,21 @@
+use adb_client::{MDNSDevice, MDNSDiscoveryService};
+use std::sync::mpsc;
+use std::time::Duration;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting mDNS device discovery...");
+
+    // Create a channel to receive discovered devices information
+    let (sender, receiver) = mpsc::channel::<MDNSDevice>();
+
+    // Create and start the discovery service
+    let mut discovery = MDNSDiscoveryService::new()?;
+    discovery.start(sender)?;
+
+    loop {
+        if let Ok(device) = receiver.recv_timeout(Duration::from_millis(100)) {
+            println!("Found device: {}", device.fullname);
+            println!("  Addresses: {:?}", device.addresses);
+        }
+    }
+}
