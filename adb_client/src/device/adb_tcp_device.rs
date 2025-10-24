88526aa94322db42c84c2432e5001f8a610fa081
Koji AGAWA <atty303@gmail.com>
2025-10-24 21:34:40 +0900

    feat(adb_client): Add support for AUTH in the TCP device

--- adb_client/src/device/adb_tcp_device.rs
+++ adb_client/src/device/adb_tcp_device.rs
@@ -1,22 +1,45 @@
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::time::Duration;
 use std::{io::Read, net::SocketAddr};
 
-use super::ADBTransportMessage;
 use super::adb_message_device::ADBMessageDevice;
 use super::models::MessageCommand;
-use crate::{ADBDeviceExt, ADBMessageTransport, ADBTransport, Result, TcpTransport};
+use super::{ADBRsaKey, ADBTransportMessage, get_default_adb_key_path};
+use crate::device::adb_transport_message::{AUTH_RSAPUBLICKEY, AUTH_SIGNATURE, AUTH_TOKEN};
+use crate::device::adb_usb_device::read_adb_private_key;
+use crate::{ADBDeviceExt, ADBMessageTransport, ADBTransport, Result, RustADBError, TcpTransport};
 
 /// Represent a device reached and available over USB.
 #[derive(Debug)]
 pub struct ADBTcpDevice {
+    private_key: ADBRsaKey,
     inner: ADBMessageDevice<TcpTransport>,
 }
 
 impl ADBTcpDevice {
     /// Instantiate a new [`ADBTcpDevice`]
     pub fn new(address: SocketAddr) -> Result<Self> {
+        Self::new_with_custom_private_key(address, get_default_adb_key_path()?)
+    }
+
+    /// Instantiate a new [`ADBTcpDevice`] using a custom private key path
+    pub fn new_with_custom_private_key(
+        address: SocketAddr,
+        private_key_path: PathBuf,
+    ) -> Result<Self> {
+        let private_key = if let Some(private_key) = read_adb_private_key(&private_key_path)? {
+            private_key
+        } else {
+            log::warn!(
+                "No private key found at path {}. Using a temporary random one.",
+                private_key_path.display()
+            );
+            ADBRsaKey::new_random()?
+        };
+
         let mut device = Self {
+            private_key,
             inner: ADBMessageDevice::new(TcpTransport::new(address)?),
         };
 
@@ -47,17 +70,68 @@ impl ADBTcpDevice {
                     .write_message(ADBTransportMessage::new(MessageCommand::Stls, 1, 0, &[]))?;
                 self.get_transport_mut().upgrade_connection()?;
                 log::debug!("Connection successfully upgraded from TCP to TLS");
+                return Ok(());
             }
             MessageCommand::Cnxn => {
                 log::debug!("Unencrypted connection established");
+                return Ok(());
+            }
+            MessageCommand::Auth => {
+                log::debug!("Authentication required");
             }
             _ => {
                 return Err(crate::RustADBError::WrongResponseReceived(
-                    "Expected CNXN or STLS command".to_string(),
+                    "Expected CNXN, STLS or AUTH command".to_string(),
                     message.header().command().to_string(),
                 ));
             }
         }
+
+        // At this point, we should have receive an AUTH message with arg0 == 1
+        let auth_message = match message.header().arg0() {
+            AUTH_TOKEN => message,
+            v => {
+                return Err(RustADBError::ADBRequestFailed(format!(
+                    "Received AUTH message with type != 1 ({v})"
+                )));
+            }
+        };
+
+        let sign = self.private_key.sign(auth_message.into_payload())?;
+
+        let message = ADBTransportMessage::new(MessageCommand::Auth, AUTH_SIGNATURE, 0, &sign);
+
+        self.get_transport_mut().write_message(message)?;
+
+        let received_response = self.get_transport_mut().read_message()?;
+
+        if received_response.header().command() == MessageCommand::Cnxn {
+            log::info!(
+                "Authentication OK, device info {}",
+                String::from_utf8(received_response.into_payload())?
+            );
+            return Ok(());
+        }
+
+        let mut pubkey = self.private_key.android_pubkey_encode()?.into_bytes();
+        pubkey.push(b'\0');
+
+        let message = ADBTransportMessage::new(MessageCommand::Auth, AUTH_RSAPUBLICKEY, 0, &pubkey);
+
+        self.get_transport_mut().write_message(message)?;
+
+        let response = self
+            .get_transport_mut()
+            .read_message_with_timeout(Duration::from_secs(10))
+            .and_then(|message| {
+                message.assert_command(MessageCommand::Cnxn)?;
+                Ok(message)
+            })?;
+
+        log::info!(
+            "Authentication OK, device info {}",
+            String::from_utf8(response.into_payload())?
+        );
 
         Ok(())
     }
