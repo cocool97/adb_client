61053c0ee5c1ed75be6b0e1756067a662eada197
Koji AGAWA <atty303@gmail.com>
2025-10-24 21:34:40 +0900

    feat(adb_client): Add support for AUTH in the TCP device

--- adb_cli/src/models/tcp.rs
+++ adb_cli/src/models/tcp.rs
@@ -1,11 +1,15 @@
 use clap::Parser;
 use std::net::SocketAddr;
+use std::path::PathBuf;
 
 use super::DeviceCommands;
 
 #[derive(Parser, Debug)]
 pub struct TcpCommand {
     pub address: SocketAddr,
+    /// Path to a custom private key to use for authentication
+    #[clap(short = 'k', long = "private-key")]
+    pub path_to_private_key: Option<PathBuf>,
     #[clap(subcommand)]
     pub commands: DeviceCommands,
 }
